ad94b39028b391ea251c7afef96216f76f269718
habibullohh <habibullohh221@gmail.com>
2022-11-21 12:35:33 +0500

    dars-5

---
+++ main.js
@@ -0,0 +1,43 @@
+// alert("Hisoblagichga hush kelibsiz")
+// let num1 = prompt("Birinchi sonni kiriting" )*1 
+// let amal =prompt ("Amalni kiriting +,-,*,/") 
+// let num2 =prompt ("Ikkinchi sonni kiriting")*1
+
+// function hisoblagich(num1,amal,num2){
+// if  (amal == '+') {
+//   return num1 + num2
+// }
+// else if (amal == '-'){
+//   return num1 - num2
+// }
+// else if (amal == '*'){
+//   return num1 * num2
+// }
+// else if (amal == '/'){
+//   return num1 / num2
+// }
+// else{
+//  return "Amalni hato kiritingiz, qayta urinib ko'ring"
+// }
+// }
+// document.write(hisoblagich(num1,amal,num2))
+// alert("Hisoblagichga hush kelibsiz")
+// let a =prompt ("1-sonni kiriting")
+// let b =prompt ("Amallardan birini kiriting +,-,*,/")
+// let c =prompt ("2-sonni kiriting")
+// // switch function hisoblagich
+// switch (b) {
+//   case "+":
+//   document.write(a + c)
+//     break
+//   case "-":
+//   document.write(a - c)
+//     break
+//   case "*":
+//     document.write(a * c)
+//     break
+//   case "/":
+//     document.write(a / c)
+//     break
+//     default: {"Amalni hato kiritingiz, qayta urinib ko'ring"}
+// }
